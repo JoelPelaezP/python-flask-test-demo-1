dc1aad7ba1dc3d4a29cf0dab10eca9ffbfcf9af3
Joel Pelaez <joelpelaez@Mac.lan>
2026-06-10 22:04:43 -0400

    format

--- database/models/item.py
+++ database/models/item.py
@@ -11,6 +11,5 @@ class ItemModel(Base):
     id = Column(Integer, primary_key=True)
     name = Column(Text, unique=True, nullable=False)
     description = Column(Text, unique=False, nullable=True)
-
     store_id = Column(Integer, ForeignKey("store.id"), unique=False, nullable=False)
     store = relationship("StoreModel", back_populates="items")
