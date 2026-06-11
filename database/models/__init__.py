6f74be93cab4772d72a84f6df57f4b6028b06773
Joel Pelaez <joelpelaez@Mac.lan>
2026-06-10 22:22:46 -0400

    Added new tables

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -3,3 +3,5 @@ from database.models.item import ItemModel
 from database.models.store import StoreModel
 from database.models.tag import TagModel
 from database.models.user import UserModel
+from database.models.customer import CustomerModel
+from database.models.invoice import InvoiceModel
